8cf15d3bdb1b38f2bb358c723712ed7379856871
Darcy Henschke <darcyhenschke@Darcys-MacBook-Air.local>
2022-07-10 12:29:56 +1000

    hamburger icon update

---
+++ Portfolio/index.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinkList = document.querySelector(".nav__link-list");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navLinkList.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
